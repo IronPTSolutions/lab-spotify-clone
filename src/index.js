6a7252683a04bdce45ac01430b42553e975f12a4
Carol Alonso <c.alonsolp@gmail.com>
2021-03-20 18:35:02 +0100

    fix: copy warning

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+console.log(`
+⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+CADA VEZ QUE COPIAS EL CÓDIGO DIOCITO MATA A UN GATITO
+              SI COPIAS T REBIENTO PAYASO
+       🥺 fuera bromis, si copias no aprendes 🥺
+⛔⛔⛔⛔⛔⛔⛔⛔⛔⛔
+`);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
